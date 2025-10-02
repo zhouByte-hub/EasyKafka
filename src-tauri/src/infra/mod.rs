2b95947a50ff1d1088c28fc78b0d17302b88cf04
zhouByte-hub <zhou_jia_ning@163.com>
2025-10-02 16:34:34 +0800

    搭建基础的rust项目架构

---
+++ src-tauri/src/infra/mod.rs
@@ -0,0 +1,2 @@
+pub mod kafka_infra;
+pub mod sql_infra;
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+pub(crate) mod kafka_error;
+pub(crate) mod handles;
+pub(crate) mod infra;
+pub(crate) mod utils;
+pub(crate) mod mcp;
+
 fn main() {
     easy_kafka_lib::run()
 }
