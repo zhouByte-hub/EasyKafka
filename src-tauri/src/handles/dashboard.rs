16b032d0b43cd2adad62385bd2e7c59eb0341f63
zhouByte-hub <zhou_jia_ning@163.com>
2025-10-05 22:34:22 +0800

    完成dashboard statistics接口，下一步需要将连接信息修改为数据库配置

--- src-tauri/src/handles/dashboard.rs
+++ src-tauri/src/handles/dashboard.rs
@@ -1,5 +1,5 @@
-use crate::{entity::response::dashboard::DashboardStatistics, EasyKafkaError};
-use rdkafka::{admin::AdminClient, util::Timeout};
+use crate::entity::response::dashboard::DashboardStatistics;
+use rdkafka::util::Timeout;
 use std::time::Duration;
 
 use crate::{infra::kafka_infra::create_kafka_client, EasyKafkaResult};
@@ -7,34 +7,39 @@ use crate::{infra::kafka_infra::create_kafka_client, EasyKafkaResult};
 #[tauri::command]
 pub fn dashboard_statistics(token: &str) -> EasyKafkaResult<DashboardStatistics> {
     let client = create_kafka_client(token)?;
-
     let mut result = DashboardStatistics::default();
+    
     let metadata = client
         .inner()
         .fetch_metadata(None, Timeout::from(Duration::from_secs(5)))?;
-    // broker count
+    // broker
     result.broker_count = metadata.brokers().len();
 
-    // topic count
+    // topic
     let topics = metadata.topics();
     result.topic_count = topics.len();
 
-    // distribution count
-    result.partition_count = if !topics.is_empty() {
-        let std_err = EasyKafkaError::StdError(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "No topic found to get partition count",
-        )));
-        topics.iter().next().ok_or(std_err)?.partitions().len()
+    if !topics.is_empty() {
+        for topic in topics {
+            let partitions = topic.partitions();
+            // partition
+            result.partition_count += partitions.len();
+            // lag
+            result.lag_count += partitions
+                .iter()
+                .map(|item| {
+                    let (high, low) = client.inner().fetch_watermarks(
+                        topic.name(),
+                        item.id(),
+                        Timeout::from(Duration::from_secs(5)),
+                    )?;
+                    Ok((high - low) as usize)
+                })
+                .sum::<EasyKafkaResult<usize>>()?;
+        }
     } else {
-        0
-    };
-    // lag count
-
+        result.partition_count = 0;
+        result.lag_count = 0;
+    }
     Ok(result)
 }
-
-// fn get_consumer_group_lag(client: &AdminClient, group_id: &str) -> EasyKafkaResult<usize> {
-   
-//     Ok(consumer_group.lag().len())
-// }
